51142094a2740c4235e63232c12cfe92f1ccae7d
Pranshuk29 <145792354+Pranshuk29@users.noreply.github.com>
2024-08-28 18:35:11 +0530

    Add files via upload

--- tree/base.py
+++ tree/base.py
@@ -7,7 +7,7 @@ You will be expected to use this to make trees for:
 > discrete input, real output
 """
 from dataclasses import dataclass
-from typing import Literal
+from typing import Union, Optional
 
 import numpy as np
 import pandas as pd
@@ -16,46 +16,108 @@ from tree.utils import *
 
 np.random.seed(42)
 
+class TreeNodeDiscrete: 
+    def __init__(self, feature, attr_val):
+        self.feature = feature
+        self.attr_val = attr_val
+        self.children = {}
+
+class TreeNodeReal:
+    def __init__(self, feature, seperation_value, attr_val):
+        self.feature = feature
+        self.attr_val = attr_val
+        self.seperation_value = seperation_value
+        self.left = 0
+        self.right = 0
+
+class LeafNode:
+    def __init__(self, pred, attr_val):
+        self.attr_val = attr_val
+        self.pred = pred
+        self.is_real = True
 
 @dataclass
 class DecisionTree:
-    criterion: Literal["information_gain", "gini_index"]  # criterion won't be used for regression
-    max_depth: int  # The maximum depth the tree can grow to
+    criterion: Union[str, None] 
+    max_depth: int 
 
-    def __init__(self, criterion, max_depth=5):
+    def __init__(self, criterion=None, max_depth=5):
         self.criterion = criterion
         self.max_depth = max_depth
+        
+
+    def helper(self, X: pd.DataFrame, y: pd.Series, features: pd.Series, depth, attr_val):
+        unique, count = np.unique(y, return_counts=True)
+        if depth == self.max_depth or unique.size == 1 or features.size == 0:
+            pred = y.mean() if check_ifreal(y) else unique[np.argmax(count)]
+            leaf = LeafNode(pred, attr_val)
+            leaf.is_real = check_ifreal(y)
+            return leaf
+
+        best_feature, seperation_value = opt_split_attribute(X, y, self.criterion, features)
+        if check_ifreal(X[best_feature]):
+            node = TreeNodeReal(best_feature, seperation_value, attr_val)
+            x_left, x_right, y_left, y_right = split_data_real(X, y, best_feature, seperation_value)
+            node.left = self.helper(x_left, y_left, features, depth+1, "Y")
+            node.right = self.helper(x_right, y_right, features, depth+1, "N")
+        else:
+            features = features[features != best_feature]
+            node = TreeNodeDiscrete(best_feature, attr_val)
+            dic_x, dic_y = split_data_discrete(X, y, best_feature)
+            for key in self.feature_values[best_feature]:
+                if key in dic_x:
+                    node.children[key] = self.helper(dic_x[key], dic_y[key], features, depth+1, key)
+                else:
+                    pred = y.mean() if check_ifreal(y) else unique[np.argmax(count)]
+                    node.children[key] = LeafNode(pred, key)
+                    node.children[key].is_real = check_ifreal(y)
+        return node
 
     def fit(self, X: pd.DataFrame, y: pd.Series) -> None:
-        """
-        Function to train and construct the decision tree
-        """
+        features = pd.Series(X.columns)
+        self.feature_values = {feature: np.unique(X[feature]) for feature in features}
+        self.root = self.helper(X, y, features, 0, "root")
 
-        # If you wish your code can have cases for different types of input and output data (discrete, real)
-        # Use the functions from utils.py to find the optimal attribute to split upon and then construct the tree accordingly.
-        # You may(according to your implemetation) need to call functions recursively to construct the tree. 
-
-        pass
+    def predict_single_point(self, X: pd.Series, node):
+        if isinstance(node, LeafNode):
+            return node.pred
+        if isinstance(node, TreeNodeDiscrete):
+            value = X[node.feature]
+            if value in node.children:
+                return self.predict_single_point(X, node.children[value])
+            else:
+                print("Error: Unexpected attribute value!")
+        else:
+            value = X[node.feature]
+            if value <= node.seperation_value:
+                return self.predict_single_point(X, node.left)
+            else:
+                return self.predict_single_point(X, node.right)
 
     def predict(self, X: pd.DataFrame) -> pd.Series:
-        """
-        Funtion to run the decision tree on test inputs
-        """
+        return X.apply(lambda instance: self.predict_single_point(instance, self.root), axis=1)
 
-        # Traverse the tree you constructed to return the predicted values for the given test inputs.
+    def print_leaf(self, node, tabs):
+        pred = f"Predicted Value is {node.pred}" if node.is_real else f"Class {node.pred}"
+        print("\t" * tabs, f"Value -> {node.attr_val}: {pred}")
 
-        pass
+    def print_discrete(self, node, tabs):
+        print("\t" * tabs, f"Value -> {node.attr_val}? (Feature -> {node.feature})")
+
+    def print_real(self, node, tabs):
+        print("\t" * tabs, f"Value -> {node.attr_val}? (Feature -> {node.feature} <= {node.seperation_value})")
+
+    def plot_helper(self, node, tabs):
+        if isinstance(node, LeafNode):
+            self.print_leaf(node, tabs)
+        elif isinstance(node, TreeNodeDiscrete):
+            self.print_discrete(node, tabs)
+            for child_node in node.children.values():
+                self.plot_helper(child_node, tabs + 1)
+        else:
+            self.print_real(node, tabs)
+            self.plot_helper(node.left, tabs + 1)
+            self.plot_helper(node.right, tabs + 1)
 
     def plot(self) -> None:
-        """
-        Function to plot the tree
-
-        Output Example:
-        ?(X1 > 4)
-            Y: ?(X2 > 7)
-                Y: Class A
-                N: Class B
-            N: Class C
-        Where Y => Yes and N => No
-        """
-        pass
+        self.plot_helper(self.root, 0)
--- tree/utils.py
+++ tree/utils.py
@@ -1,77 +1,207 @@
-"""
-You can add your own functions here according to your decision tree implementation.
-There is no restriction on following the below template, these fucntions are here to simply help you.
-"""
-
 import pandas as pd
-
-def one_hot_encoding(X: pd.DataFrame) -> pd.DataFrame:
-    """
-    Function to perform one hot encoding on the input data
-    """
-
-    pass
+import numpy as np
 
 def check_ifreal(y: pd.Series) -> bool:
     """
     Function to check if the given series has real or discrete values
     """
+    assert isinstance(y, pd.Series), "Input must be a pandas Series."
+    return y.dtype in [float, int]
 
-    pass
 
-
-def entropy(Y: pd.Series) -> float:
+def entropy(y: pd.Series) -> float:
     """
     Function to calculate the entropy
     """
+    unique, counts = np.unique(y, return_counts=True)
+    total = y.size
+    result = 0
+    for i in range(len(unique)):
+        proportion = counts[i] / total
+        result -= proportion * np.log(proportion)
+    return result
 
-    pass
 
-
-def gini_index(Y: pd.Series) -> float:
+def gini_index(y: pd.Series) -> float:
     """
     Function to calculate the gini index
     """
+    unique, counts = np.unique(y, return_counts=True)
+    total = y.size
+    result = 0
+    for i in range(len(unique)):
+        proportion = counts[i] / total
+        result += proportion * (1 - proportion)
+    return result
 
-    pass
+
+def gini_discrete_input(y: pd.Series, attr: pd.Series) -> float:
+    unique_values = np.unique(attr)
+    result = 0
+    for i in unique_values:
+        y_i = y[attr == i]
+        result += (y_i.size / y.size) * gini_index(y_i)
+    return result
 
 
-def information_gain(Y: pd.Series, attr: pd.Series, criterion: str) -> float:
+def gini_real_input(y: pd.Series, attr: pd.Series, value) -> float:
+    y_left = y[attr <= value]
+    y_right = y[attr > value]
+    gini_left = gini_index(y_left) * (y_left.size / y.size)
+    gini_right = gini_index(y_right) * (y_right.size / y.size)
+    return gini_left + gini_right
+
+
+def information_gain_discrete_input(y: pd.Series, attr: pd.Series) -> float:
     """
-    Function to calculate the information gain using criterion (entropy, gini index or MSE)
+    Function to calculate the information gain for discrete input
     """
+    assert y.size == attr.size
+    initial_entropy = entropy(y)
+    final_entropy = 0
+    unique = np.unique(attr)
+    for i in unique:
+        y_i = y[attr == i]
+        final_entropy += (y_i.size / y.size) * entropy(y_i)
+    info_gain = initial_entropy - final_entropy
+    return info_gain
 
-    pass
+
+def information_gain_real_input(y: pd.Series, attr: pd.Series, value) -> float:
+    """
+    Function to calculate the information gain for real input
+    """
+    assert y.size == attr.size
+    initial_entropy = entropy(y)
+    y_left = y[attr <= value]
+    y_right = y[attr > value]
+    final_entropy = (y_left.size / y.size) * entropy(y_left) + (y_right.size / y.size) * entropy(y_right)
+    info_gain = initial_entropy - final_entropy
+    return info_gain
 
 
-def opt_split_attribute(X: pd.DataFrame, y: pd.Series, criterion, features: pd.Series):
+def mse(y: pd.Series) -> float:
+    """
+    Function to calculate the mean squared error (MSE)
+    """
+    mean_value = y.mean()
+    result = ((y - mean_value) ** 2).mean()
+    return result
+
+
+def reduction_mse_discrete_input(y: pd.Series, attr: pd.Series) -> float:
+    unique_values = np.unique(attr)
+    initial_mse = mse(y)
+    result = 0
+    for i in unique_values:
+        y_i = y[attr == i]
+        result += (y_i.size / y.size) * mse(y_i)
+    return initial_mse - result
+
+
+def reduction_mse_real_input(y: pd.Series, attr: pd.Series, value) -> float:
+    initial_mse = mse(y)
+    y_left = y[attr <= value]
+    y_right = y[attr > value]
+    mse_left = mse(y_left) * (y_left.size / y.size)
+    mse_right = mse(y_right) * (y_right.size / y.size)
+    return initial_mse - (mse_left + mse_right)
+
+
+def opt_split_reduction_mse(X: pd.DataFrame, y: pd.Series, features: pd.Series):
+    max_reduction = float('-inf')
+    best_feature = None
+    best_value = None
+    for feature in features:
+        if check_ifreal(X[feature]):
+            unique_values = np.unique(X[feature])
+            sorted_values = (unique_values[:-1] + unique_values[1:]) / 2
+            for value in sorted_values:
+                current_reduction = reduction_mse_real_input(y, X[feature], value)
+                if current_reduction > max_reduction:
+                    max_reduction = current_reduction
+                    best_feature = feature
+                    best_value = value
+        else:
+            current_reduction = reduction_mse_discrete_input(y, X[feature])
+            if current_reduction > max_reduction:
+                max_reduction = current_reduction
+                best_feature = feature
+                best_value = None
+    return best_feature, best_value
+
+
+def opt_split_information_gain(X: pd.DataFrame, y: pd.Series, features: pd.Series):
+    max_info_gain = float('-inf')
+    best_feature = None
+    best_value = None
+    for feature in features:
+        if check_ifreal(X[feature]):
+            unique_values = np.unique(X[feature])
+            sorted_values = (unique_values[:-1] + unique_values[1:]) / 2
+            for value in sorted_values:
+                current_info_gain = information_gain_real_input(y, X[feature], value)
+                if current_info_gain > max_info_gain:
+                    max_info_gain = current_info_gain
+                    best_feature = feature
+                    best_value = value
+        else:
+            current_info_gain = information_gain_discrete_input(y, X[feature])
+            if current_info_gain > max_info_gain:
+                max_info_gain = current_info_gain
+                best_feature = feature
+                best_value = None
+    return best_feature, best_value
+
+
+def opt_split_gini_index(X: pd.DataFrame, y: pd.Series, features: pd.Series):
+    min_gini = float('inf')
+    best_feature = None
+    best_value = None
+    for feature in features:
+        if check_ifreal(X[feature]):
+            unique_values = np.unique(X[feature])
+            sorted_values = (unique_values[:-1] + unique_values[1:]) / 2
+            for value in sorted_values:
+                current_gini = gini_real_input(y, X[feature], value)
+                if current_gini < min_gini:
+                    min_gini = current_gini
+                    best_feature = feature
+                    best_value = value
+        else:
+            current_gini = gini_discrete_input(y, X[feature])
+            if current_gini < min_gini:
+                min_gini = current_gini
+                best_feature = feature
+                best_value = None
+    return best_feature, best_value
+
+
+def opt_split_attribute(X: pd.DataFrame, y: pd.Series, criterion: str, features: pd.Series):
     """
     Function to find the optimal attribute to split about.
-    If needed you can split this function into 2, one for discrete and one for real valued features.
-    You can also change the parameters of this function according to your implementation.
-
-    features: pd.Series is a list of all the attributes we have to split upon
-
-    return: attribute to split upon
     """
+    if check_ifreal(y):
+        return opt_split_reduction_mse(X, y, features)
+    elif criterion == "information_gain":
+        return opt_split_information_gain(X, y, features)
+    else:
+        return opt_split_gini_index(X, y, features)
 
-    # According to wheather the features are real or discrete valued and the criterion, find the attribute from the features series with the maximum information gain (entropy or varinace based on the type of output) or minimum gini index (discrete output).
 
-    pass
+def split_data_real(X: pd.DataFrame, y: pd.Series, attribute, value):
+    x_left = X[X[attribute] <= value]
+    x_right = X[X[attribute] > value]
+    y_left = y[X[attribute] <= value]
+    y_right = y[X[attribute] > value]
+    return x_left, x_right, y_left, y_right
 
 
-def split_data(X: pd.DataFrame, y: pd.Series, attribute, value):
-    """
-    Funtion to split the data according to an attribute.
-    If needed you can split this function into 2, one for discrete and one for real valued features.
-    You can also change the parameters of this function according to your implementation.
-
-    attribute: attribute/feature to split upon
-    value: value of that attribute to split upon
-
-    return: splitted data(Input and output)
-    """
-
-    # Split the data based on a particular value of a particular attribute. You may use masking as a tool to split the data.
-
-    pass
+def split_data_discrete(X: pd.DataFrame, y: pd.Series, split_attribute):
+    unique_values = np.unique(X[split_attribute])
+    dic_x = {}
+    dic_y = {}
+    for value in unique_values:
+        dic_x[value] = X[X[split_attribute] == value]
+        dic_y[value] = y[X[split_attribute] == value]
+    return dic_x, dic_y
